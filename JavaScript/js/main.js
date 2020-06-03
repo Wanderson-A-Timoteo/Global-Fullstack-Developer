01e526493f2d8151aab597d02f79534022f969ae
Wanderson Timóteo <wanderson_timoteo@hotmail.com>
2020-06-02 22:59:26 -0400

    criado pasta JavaScript e os arq. index.html e main.js

---
+++ JavaScript/js/main.js
@@ -0,0 +1,17 @@
+
+var nome = "Wanderson Timóteo";
+var idade = 36;
+var idade2 = 10;
+var frase = "Japão é o melhor time do mundo";
+var n1 = 5, n2 = 3;
+//alert(nome + " tem " + idade + " anos ");
+//alert(idade + idade2);
+
+console.log(nome);
+console.log(idade + idade2);
+console.log(frase);
+console.log(frase.replace("Japão", "Brasil"));
+//alert(frase.replace("Japão", "Brasil"));
+console.log(frase.toUpperCase());
+console.log(frase.toLowerCase());
+console.log(n1 * n2);
