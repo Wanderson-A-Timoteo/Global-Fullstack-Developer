78b5e81c66f1d469ec796c3926813790c903e636
Wanderson-A-Timoteo <wanderson_timoteo@hotmail.com>
2020-07-03 19:16:23 -0400

    criado pasta Python e movidos todos os projetos python para ela

--- app_python/aula12.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-response = requests.get('https://viacep.com.br/ws/01001000/json/')
-print(response.status_code)
-print(response.text)
-print(response.json())
-print('\n')
-dados_cep = response.json()
-print(dados_cep['logradouro'])
